d1f65a68cf479ac482d3b5e8e29acece99286eae
Andrew <voland62@gmail.com>
2020-02-09 18:21:39 +0200

    minor...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Namatyvanie.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Namatyvanie.java
@@ -2,31 +2,15 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
 
-/**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
 @TeleOp(name="Lift", group="Linear Opmode")
 
 public class Namatyvanie extends LinearOpMode {
 
-    // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor liftDrive = null;
 
@@ -39,8 +23,6 @@ public class Namatyvanie extends LinearOpMode {
 
         waitForStart();
         runtime.reset();
-
-
 
         while(opModeIsActive()){
 
